9580ff72d61bcfb901f5279f6ab66f9a2e2a6425
Pasha <89598765+pasha359@users.noreply.github.com>
2023-03-19 20:30:46 +0300

    Add files via upload

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Wood, Keyword
+from .models import Category, Wood, Keyword, Photo
 
 class WoodAdmin(admin.StackedInline):
     model = Wood
@@ -13,8 +13,15 @@ class KeywordAdmin(admin.ModelAdmin):
     model = Keyword
     list_display = ['name', 'published']
 
+class PhotoAdmin(admin.ModelAdmin):
+    model = Photo
+    list_display = ['name', 'image']
+
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Keyword, KeywordAdmin)
+admin.site.register(Photo, PhotoAdmin)
 
 
 
